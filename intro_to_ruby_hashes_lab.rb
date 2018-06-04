665db0bccfdafa7b4aa9c0beb1d2fc292bd31ac5
Peter Kim <peterckim96@gmail.com>
2018-06-04 04:13:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,13 +38,12 @@ def monopoly_with_third_tier
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25, 
-	      :two_pieces:owned => 50,
+	      :two_pieces_owned => 50,
 	      :three_pieces_owned => 100,
 	      :four_pieces_owned => 200
 	    }
 	  }
 	}
-		
 end
 
 def monopoly_with_fourth_tier
